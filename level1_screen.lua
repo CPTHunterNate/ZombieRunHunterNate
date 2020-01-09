a9780ac285b45f975f3bd1bdbbe5d1e2916b9869
Nate Day <55556056+s445276@users.noreply.github.com>
2020-01-09 14:15:19 -0500

    push

--- level1_screen.lua
+++ level1_screen.lua
@@ -790,10 +790,12 @@ function scene:hide( event )
 
     elseif ( phase == "did" ) then
         
+        
+        physics.stop()
+
         RemoveArrowEventListeners()
         RemoveRuntimeListeners()
         display.remove(character)
-        physics.stop()
 
         Runtime:removeEventListener("enterFrame", MoveZombie1)
         Runtime:removeEventListener("enterFrame", MoveZombie2)
